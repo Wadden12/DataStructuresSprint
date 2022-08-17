ad5f27e203f1a582abf22a098c7128b51808d8f5
Mike Wadden <mike.wadden@keyin.com>
2022-08-17 07:07:46 -0230

    project framed

---
+++ Project/user/src/components/InputForum.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InputForum = () => {
+  return (
+    <div>
+        <h1>Data Structure Sprint AVL Tree</h1>
+        <form> 
+            <label>Input a list of numbers</label>
+            <input></input>
+        </form>
+    </div>
+  )
+}
+
+export default  InputForum
